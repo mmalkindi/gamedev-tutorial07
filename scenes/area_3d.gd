ed232799eed98092686bdeff33c4c89319c6f2d5
mma <111668501+mmalkindi@users.noreply.github.com>
2025-03-27 16:39:12 +0700

    polish: show mouse when switched to win screen

--- scenes/area_3d.gd
+++ scenes/area_3d.gd
@@ -5,4 +5,6 @@ extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.get_name() == "Player":
+		if change_to_scene == "WinScreen":
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().change_scene_to_file(str("res://scenes/" + change_to_scene + ".tscn"))
